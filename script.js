94e42c25e3ba442d98bb9b5b20881541cc240420
ANT D LISBOA <Antdlisboa@gmail.com>
2025-05-28 23:05:28 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 let highestZ = 1;
-
 class Paper {
   holdingPaper = false;
   mouseTouchX = 0;
@@ -23,20 +22,17 @@ class Paper {
         this.velX = this.mouseX - this.prevMouseX;
         this.velY = this.mouseY - this.prevMouseY;
       }
-
       const dirX = e.clientX - this.mouseTouchX;
       const dirY = e.clientY - this.mouseTouchY;
-      const dirLength = Math.sqrt(dirX * dirX + dirY * dirY);
+      const dirLength = Math.sqrt(dirX * dirX + dirY * dirY) || 1;
       const dirNormalizedX = dirX / dirLength;
       const dirNormalizedY = dirY / dirLength;
       const angle = Math.atan2(dirNormalizedY, dirNormalizedX);
       let degrees = (180 * angle) / Math.PI;
       degrees = (360 + Math.round(degrees)) % 360;
-
       if (this.rotating) {
         this.rotation = degrees;
       }
-
       if (this.holdingPaper) {
         if (!this.rotating) {
           this.currentPaperX += this.velX;
@@ -44,25 +40,20 @@ class Paper {
         }
         this.prevMouseX = this.mouseX;
         this.prevMouseY = this.mouseY;
-
         paper.style.transform = `translateX(${this.currentPaperX}px) translateY(${this.currentPaperY}px) rotateZ(${this.rotation}deg)`;
       }
     });
 
     paper.addEventListener("mousedown", (e) => {
       if (this.holdingPaper) return;
-
       this.holdingPaper = true;
-      paper.style.zIndex = highestZ;
-      highestZ += 1;
-
+      paper.style.zIndex = highestZ++;
       if (e.button === 0) {
         this.mouseTouchX = this.mouseX;
         this.mouseTouchY = this.mouseY;
         this.prevMouseX = this.mouseX;
         this.prevMouseY = this.mouseY;
       }
-
       if (e.button === 2) {
         this.rotating = true;
       }
@@ -71,40 +62,6 @@ class Paper {
     window.addEventListener("mouseup", () => {
       this.holdingPaper = false;
       this.rotating = false;
-    });
-
-    paper.addEventListener("contextmenu", (e) => e.preventDefault());
-
-    // Eventos para toque
-    paper.addEventListener("touchstart", (e) => {
-      e.preventDefault();
-      const touch = e.touches[0];
-      this.mouseX = touch.clientX;
-      this.mouseY = touch.clientY;
-      this.mouseTouchX = this.mouseX;
-      this.mouseTouchY = this.mouseY;
-      this.prevMouseX = this.mouseX;
-      this.prevMouseY = this.mouseY;
-      this.holdingPaper = true;
-      paper.style.zIndex = highestZ++;
-    });
-
-    document.addEventListener("touchmove", (e) => {
-      if (!this.holdingPaper) return;
-      const touch = e.touches[0];
-      this.mouseX = touch.clientX;
-      this.mouseY = touch.clientY;
-      this.velX = this.mouseX - this.prevMouseX;
-      this.velY = this.mouseY - this.prevMouseY;
-      this.currentPaperX += this.velX;
-      this.currentPaperY += this.velY;
-      this.prevMouseX = this.mouseX;
-      this.prevMouseY = this.mouseY;
-      paper.style.transform = `translateX(${this.currentPaperX}px) translateY(${this.currentPaperY}px) rotateZ(${this.rotation}deg)`;
-    });
-
-    document.addEventListener("touchend", () => {
-      this.holdingPaper = false;
     });
   }
 }
@@ -115,3 +72,4 @@ papers.forEach((paper) => {
   p.init(paper);
 });
 
+window.addEventListener("contextmenu", (e) => e.preventDefault());
