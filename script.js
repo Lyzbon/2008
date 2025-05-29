d8790837e39fa7a8937aaca5d670250be401fa5d
ANT D LISBOA <Antdlisboa@gmail.com>
2025-05-28 23:24:49 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,83 @@
-const colors = ['red', 'blue', 'yellow', 'green', 'purple', 'pink'];
-function createBalloon() {
-  const balloon = document.createElement('div');
-  balloon.classList.add('balloon');
-  balloon.style.left = Math.random() * 100 + 'vw';
-  balloon.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
-  balloon.style.width = '20px';
-  balloon.style.height = '30px';
-  document.body.appendChild(balloon);
-  setTimeout(() => balloon.remove(), 6000);
+
+let highestZ = 1;
+
+class Paper {
+  holdingPaper = false;
+  mouseTouchX = 0;
+  mouseTouchY = 0;
+  mouseX = 0;
+  mouseY = 0;
+  prevMouseX = 0;
+  prevMouseY = 0;
+  velX = 0;
+  velY = 0;
+  rotation = Math.random() * 30 - 15;
+  currentPaperX = 0;
+  currentPaperY = 0;
+  rotating = false;
+
+  init(paper) {
+    document.addEventListener("mousemove", (e) => {
+      if (!this.rotating) {
+        this.mouseX = e.clientX;
+        this.mouseY = e.clientY;
+        this.velX = this.mouseX - this.prevMouseX;
+        this.velY = this.mouseY - this.prevMouseY;
+      }
+
+      const dirX = e.clientX - this.mouseTouchX;
+      const dirY = e.clientY - this.mouseTouchY;
+      const dirLength = Math.sqrt(dirX * dirX + dirY * dirY);
+      const dirNormalizedX = dirX / dirLength;
+      const dirNormalizedY = dirY / dirLength;
+      const angle = Math.atan2(dirNormalizedY, dirNormalizedX);
+      let degrees = (180 * angle) / Math.PI;
+      degrees = (360 + Math.round(degrees)) % 360;
+
+      if (this.rotating) {
+        this.rotation = degrees;
+      }
+
+      if (this.holdingPaper) {
+        if (!this.rotating) {
+          this.currentPaperX += this.velX;
+          this.currentPaperY += this.velY;
+        }
+
+        this.prevMouseX = this.mouseX;
+        this.prevMouseY = this.mouseY;
+
+        paper.style.transform = `translateX(${this.currentPaperX}px) translateY(${this.currentPaperY}px) rotateZ(${this.rotation}deg)`;
+      }
+    });
+
+    paper.addEventListener("mousedown", (e) => {
+      if (this.holdingPaper) return;
+      this.holdingPaper = true;
+      paper.style.zIndex = highestZ;
+      highestZ += 1;
+
+      if (e.button === 0) {
+        this.mouseTouchX = this.mouseX;
+        this.mouseTouchY = this.mouseY;
+        this.prevMouseX = this.mouseX;
+        this.prevMouseY = this.mouseY;
+      }
+
+      if (e.button === 2) {
+        this.rotating = true;
+      }
+    });
+
+    window.addEventListener("mouseup", () => {
+      this.holdingPaper = false;
+      this.rotating = false;
+    });
+  }
 }
-setInterval(createBalloon, 500);
 
-document.getElementById('playButton').addEventListener('click', function () {
-  document.getElementById('bgm').play();
-  this.style.display = 'none';
-});
-
-document.getElementById('confirmButton').addEventListener('click', function () {
-  document.getElementById('confirmationMessage').style.display = 'block';
-  this.disabled = true;
-  this.textContent = 'Presença Confirmada';
-  this.style.backgroundColor = '#aaa';
+const papers = Array.from(document.querySelectorAll(".paper"));
+papers.forEach((paper) => {
+  const p = new Paper();
+  p.init(paper);
 });
