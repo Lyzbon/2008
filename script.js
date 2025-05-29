91ffb1b3f783b6c773f4927664f82541bf118a5f
ANT D LISBOA <Antdlisboa@gmail.com>
2025-05-29 00:22:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-
 let highestZ = 1;
-
 class Paper {
   holdingPaper = false;
   mouseTouchX = 0;
@@ -15,7 +13,6 @@ class Paper {
   currentPaperX = 0;
   currentPaperY = 0;
   rotating = false;
-
   init(paper) {
     document.addEventListener("mousemove", (e) => {
       if (!this.rotating) {
@@ -24,7 +21,6 @@ class Paper {
         this.velX = this.mouseX - this.prevMouseX;
         this.velY = this.mouseY - this.prevMouseY;
       }
-
       const dirX = e.clientX - this.mouseTouchX;
       const dirY = e.clientY - this.mouseTouchY;
       const dirLength = Math.sqrt(dirX * dirX + dirY * dirY);
@@ -33,11 +29,9 @@ class Paper {
       const angle = Math.atan2(dirNormalizedY, dirNormalizedX);
       let degrees = (180 * angle) / Math.PI;
       degrees = (360 + Math.round(degrees)) % 360;
-
       if (this.rotating) {
         this.rotation = degrees;
       }
-
       if (this.holdingPaper) {
         if (!this.rotating) {
           this.currentPaperX += this.velX;
@@ -45,39 +39,30 @@ class Paper {
         }
         this.prevMouseX = this.mouseX;
         this.prevMouseY = this.mouseY;
-
         paper.style.transform = `translateX(${this.currentPaperX}px) translateY(${this.currentPaperY}px) rotateZ(${this.rotation}deg)`;
       }
     });
-
     paper.addEventListener("mousedown", (e) => {
       if (this.holdingPaper) return;
-
       this.holdingPaper = true;
       paper.style.zIndex = highestZ;
       highestZ += 1;
-
       if (e.button === 0) {
         this.mouseTouchX = this.mouseX;
         this.mouseTouchY = this.mouseY;
         this.prevMouseX = this.mouseX;
         this.prevMouseY = this.mouseY;
       }
-
       if (e.button === 2) {
         this.rotating = true;
       }
     });
-
     window.addEventListener("mouseup", () => {
       this.holdingPaper = false;
       this.rotating = false;
     });
-
-    paper.addEventListener("contextmenu", (e) => e.preventDefault());
   }
 }
-
 const papers = Array.from(document.querySelectorAll(".paper"));
 papers.forEach((paper) => {
   const p = new Paper();
